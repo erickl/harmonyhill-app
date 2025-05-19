54e40b8b201ee8f8aea02d01b21391fd144187e1
Eric Klaesson <ericklaesson@gmail.com>
2025-05-19 08:42:11 +0700

    disabled app check for now

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import { getAuth } from "firebase/auth";
-import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
+//import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check"; //DebugAppCheckProvider
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,15 +21,19 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// 2025-05-15: unenforced currently. Can be enforced in the Firebase console
+// todo: includ the final domain in the google console, in the enterprise recaptcha security section: https://console.cloud.google.com/security/recaptcha/create?inv=1&invt=AbxtoQ&project=harmonyhill-1
+// const appCheck = initializeAppCheck(app, {
+//     provider: new ReCaptchaEnterpriseProvider(process.env.REACT_APP_RECAPTCHA_SITE_KEY),
+//     //provider: new DebugAppCheckProvider(),
+//     isTokenAutoRefreshEnabled: true,
+// });
+
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
-
-// 2025-05-15: unenforced currently. Can be enforced in the Firebase console
-initializeAppCheck(app, {
-    provider: new ReCaptchaEnterpriseProvider(process.env.REACT_APP_RECAPTCHA_SITE_KEY),
-    isTokenAutoRefreshEnabled: true,
-});
+const storage = getStorage(app, firebaseConfig.storageBucket);
 
 
-export { db, auth, analytics };
+export { db, auth, analytics, storage };
