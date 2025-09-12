f7ceefc1a808f9def3c41d0affe47738497874fb
Eric Klaesson <ericklaesson@gmail.com>
2025-09-12 10:35:57 +0800

    add size input to loading spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,5 +1,16 @@
 import "./Spinner.css";
 
-export default function Spinner() {
-    return <div className="spinner" />;
+export default function Spinner({size}) {
+    size = size ? size : 32;
+    const style = {
+        "border": "4px solid rgba(0, 0, 0, 0.1)",
+        "borderLeftColor": "#09f",
+        "borderRadius": "50%",
+        "width": `${size}px`,
+        "height": `${size}px`,
+        "animation": "spin 1s linear infinite",
+        "margin": "auto",
+    }
+
+    return <div style={style}/>;
 }
