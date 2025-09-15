31cb02dd6360cebf3aecae665ff83ec6b6e72e26
Eric Klaesson <ericklaesson@gmail.com>
2025-09-15 14:15:59 +0800

    bugfix: correct date picker popping out beyond the screen limit

--- src/components/MyDatePicker.js
+++ src/components/MyDatePicker.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { TextField, Checkbox, FormControlLabel } from '@mui/material';
 import { DateTime } from 'luxon';
-import { Utensils } from 'lucide-react';
 import * as utils from "../utils";
+import "./MyDatePicker.css";
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
 
 export default function MyDatePicker({ name, date, time, onChange, useTime }) {
     //const isMidnight = (dt) => dt !== null && dt.hour === 0 && dt.minute === 0 && dt.second === 0 && dt.millisecond === 0;
@@ -14,8 +14,8 @@ export default function MyDatePicker({ name, date, time, onChange, useTime }) {
     date = utils.toDateTime(date);
     time = utils.toDateTime(time);
 
-    const [startingTime, setStartingTime] = useState(time);
-    const [startingDate, setStartingDate] = useState(date);
+    const [startingTime, setTime] = useState(time);
+    const [startingDate, setDate] = useState(date);
 
     const handleTimeChange = (newTime) => {
         const newTimeCopy = newTime.setZone(utils.getHotelTimezone(), { keepLocalTime: true });
@@ -30,8 +30,8 @@ export default function MyDatePicker({ name, date, time, onChange, useTime }) {
             });
         }
 
-        setStartingDate(newStartingDate);
-        setStartingTime(newTimeCopy);
+        setDate(newStartingDate);
+        setTime(newTimeCopy);
 
         onChange("_batch", {
             [name]: newStartingDate,
@@ -51,35 +51,45 @@ export default function MyDatePicker({ name, date, time, onChange, useTime }) {
                 });
             }
 
-            setStartingDate(newDateCopy);
+            setDate(newDateCopy);
             onChange(name, newDateCopy);
         }
     };
 
-    return ( <>
-        <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="en-gb">
-            <DateTimePicker
-                label="Select a date"
-                value={date}
-                format="dd/MM/yyyy"
-                onChange={handleDateChange}
-                renderInput={(params) => <TextField {...params} fullWidth />}
-                ampm={false}
-                views={['year', 'month', 'day']}
-            />
-        </LocalizationProvider>
-        
-        {useTime !== false && (
-            <LocalizationProvider dateAdapter={AdapterLuxon}>
-                <TimePicker
-                    label="Select a time"
-                    value={startingTime}
-                    onChange={handleTimeChange}
+    return (
+        <div style={{ display: "flex", gap: "16px" }}>
+            <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="en-gb">
+                <MobileDateTimePicker
+                    slotProps={{
+                        textField: { fullWidth: true }
+                    }}  
+                    label="Select a date"
+                    value={date}
+                    format="dd/MM/yyyy"
+                    onChange={(newDate) => handleDateChange(newDate)}
                     renderInput={(params) => <TextField {...params} fullWidth />}
                     ampm={false}
-                    views={['hours', 'minutes']}
+                    views={["year", "month", "day"]}
                 />
             </LocalizationProvider>
-        )}
-    </>);
+
+            {useTime !== false && (<>
+                <div style={{ marginTop: "8px" }} />
+                <LocalizationProvider dateAdapter={AdapterLuxon}>
+                    <MobileTimePicker
+                        label="Select a time"
+                        value={startingTime}
+                        onChange={(newTime) => handleTimeChange(newTime)}
+                        renderInput={(params) => <TextField {...params} fullWidth />}
+                        ampm={false}
+                        views={["hours", "minutes"]}
+                        desktopModeMediaQuery="(max-width: 999999px)" // force modal mode
+                        slotProps={{
+                            textField: { fullWidth: true }
+                        }}  
+                    />
+                </LocalizationProvider>
+            </>)}
+        </div>
+    );
 }
