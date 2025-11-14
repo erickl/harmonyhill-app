836d852357f2c65892e9f187d1a72708b5aecb0d
Eric Klaesson <ericklaesson@gmail.com>
2025-11-14 16:16:59 -0500

    create inventory month closing count services

--- src/daos/inventoryDao.js
+++ src/daos/inventoryDao.js
@@ -1,11 +1,66 @@
 import * as utils from "../utils.js";
 import * as dao from "../daos/dao.js";
 
+export async function getOne(nameOrId, onError) {
+    const id = nameOrId.startsWith("inv-") ? nameOrId : makeInventoryItemId(name);
+    const path = ["inventory"];
+    return await dao.getOne(path, id, onError);
+}
+
 export async function add(object, onError) {
     const inventoryItemId = makeInventoryItemId(object.name);
     const docId = makeId(object.type, inventoryItemId);
     const path = ["inventory", inventoryItemId, "stock"];
     return await dao.add(path, docId, object, onError);
+}
+
+export async function getInventoryChanges(name, filterOptions) {
+    const invItem = getOne(name, onError);
+    if(!invItem) return [];
+
+    const queryFilter = [];
+
+    if (utils.exists(filterOptions, "type")) {
+        queryFilter.push(where("type", "==", filterOptions.type));
+    }
+
+    if (utils.exists(filterOptions, "after")) {
+        const afterDateFireStore = utils.toFireStoreTime(filterOptions.after);
+        queryFilter.push(where("createdAt", ">=", afterDateFireStore));
+    }
+
+    if (utils.exists(filterOptions, "before")) {
+        const beforeDateFireStore = utils.toFireStoreTime(filterOptions.before);
+        queryFilter.push(where("createdAt", "<=", beforeDateFireStore));
+    }
+
+    let ordering = [ orderBy("createdAt", "asc") ];
+    return await dao.get(path, queryFilter, ordering, -1, onError);
+}
+
+export async function getLastClosedRecord(name, onError) {
+    const invItem = await getOne(name, onError);
+    if(!invItem) return null;
+
+    const path = ["inventory", invItem.id, `${invItem.id}-closed`];
+    const newestFirst = [ orderBy("closedAt", "desc") ];
+    
+    const lastClosedRecords = await dao.get(path, [], newestFirst, 1, onError);
+    if(!lastClosedRecords || lastClosedRecords.length < 1) return null;
+
+    const lastClosedRecord = lastClosedRecords[0];
+    return lastClosedRecord;
+}
+
+export async function addClosedRecord(name, data, onError) {
+    const invItemId = makeInventoryItemId(name);
+
+    const previousMonthShort = data.closedAt.monthShort.toLowerCase();
+    const yearYY = data.closedAt.year - 2000;
+    const id = `${invItemId}-closed-${previousMonthShort}-${yearYY}`;
+    const path = ["inventory", invItemId, "inv-closed"];
+    
+    return await dao.add(path, id, data, onError);
 }
 
 export function makeId(type, inventoryItemId) {
--- src/services/inventoryService.js
+++ src/services/inventoryService.js
@@ -1,4 +1,5 @@
 import * as inventoryDao from "../daos/inventoryDao.js";
+import * as utils from "../utils.js";
 
 export async function addSale(booking, itemName, quantity, onError) {
     const sale = {
@@ -17,9 +18,69 @@ export async function refill(expense, itemName, quantity, onError) {
         expense  : expense.id,
         name     : itemName,
         quantity : quantity,
+        seller   : utils.isEmpty(expense.seller) ? "" : expense.seller,
         type     : "refill",
     };
     
     const result = await inventoryDao.add(refill, onError);
+    return result;
+}
+
+export async function getLastClosedRecord(name, onError) {
+    const lastClosedRecord = await inventoryDao.getLastClosedRecord(name, onError);
+    return lastClosedRecord;
+}
+
+export async function getSales(name, filters, onError) {
+    filters.type = "sale";
+    const sales = await inventoryDao.getInventoryChanges(name, filters, onError);
+    return sales;
+}
+
+export async function getRefills(name, filters, onError) {
+    filters.type = "refill";
+    const purchases = await inventoryDao.getInventoryChanges(name, filters, onError);
+    return purchases;
+}
+
+/**
+ * 
+ * @param {*} name of the inventory item 
+ * @param {*} filter: Include "before" and date, to return quantity until a certain date
+ * @param {*} onError 
+ * @returns the amount of remaining stock of the named inventory item
+ */
+export async function getQuantity(name, filter, onError) {
+    if(!utils.exists(filter, "after")) {
+        const lastClosedRecord = await getLastClosedRecord(name, onError);
+        if(lastClosedRecord) {
+            filter.after = lastClosedRecord.closedAt;
+        }
+    }  
+
+    const sales = await getSales(name, filter, onError);
+    const totalSales = sales.reduce((sum, sale) => sum + sale.quantity, 0);
+
+    const refills = await getRefills(name, filter, onError);
+    const totalRefills = refills.reduce((sum, refill) => sum + refill.quantity, 0);
+
+    const currentCount = lastClosedRecord.count + totalRefills - totalSales;
+    return currentCount;
+}
+
+export async function closeMonthForItem(name, onError) {
+    const lastClosedRecord = await getLastClosedRecord(name, onError);
+    if(!lastClosedRecord) return false;
+
+    const newClosedAt = utils.monthStart().plus({seconds : -1});
+
+    const totalMonthCount = await getQuantity(name, {"before" : newClosedAt}, onError);
+
+    const newLastClosedRecord = {
+        "count" : totalMonthCount,
+        "closedAt" : newClosedAt,
+    };
+
+    const result = await inventoryDao.addClosedRecord(name, newLastClosedRecord, onError);
     return result;
 }
