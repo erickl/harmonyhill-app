1622ea77a9e59e2604e85ee2f493b928affdaa10
Eric Klaesson <ericklaesson@gmail.com>
2025-11-16 21:53:41 -0500

    add buttons & functionality to display & add sales and refills for each inventory item

--- src/components/InventoryScreen.js
+++ src/components/InventoryScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import * as inventoryService from "../services/inventoryService.js";
 import * as userService from "../services/userService.js";
+import * as bookingService from "../services/bookingService.js";
+import * as expenseService from "../services/expenseService.js";
 import * as utils from "../utils.js";
 import { useNotification } from "../context/NotificationContext.js";
 import MetaInfo from './MetaInfo.js';
-import { Receipt, ShoppingCart, Trash2, ImageDown } from 'lucide-react';
+import { Receipt, ShoppingCart, PlusCircle, MinusCircle } from 'lucide-react';
 import "./InventoryScreen.css";
 import Spinner from "./Spinner.js";
 import { useDataTableModal } from '../context/DataTableContext.js';
@@ -26,6 +28,57 @@ export default function InventoryScreen({onNavigate, onClose}) {
         setLoadingExpanded((prev) => ({...prev, [item.id]: true}));
         await fetchItemInfo(item);
         setLoadingExpanded((prev) => ({...prev, [item.id]: false}));
+    }
+
+    const onAddStock = async(item) => {
+        onNavigate("addInventory", {item});
+    }
+
+    const onSubtractStock = async(item) => {
+        onNavigate("subtractInventory", {item});
+    }
+
+    const onDisplaySalesData = async() => {    
+        const headers = ["", "date", "quantity", "booking", "villa", "soldBy"];  
+        const enhancedSales = [];
+
+        for(let i = 0; i < sales.length; i++) {
+            const sale = sales[i];
+            const booking = await bookingService.getOne(sale.bookingId);
+            
+            const enhancedSale = [
+                i+1,
+                utils.to_ddMMM(sale.createdAt),
+                sale.quantity,
+                booking.name,
+                booking.house.trim().toLowerCase() === "harmony hill" ? "HH" : "JN",
+                sale.createdBy,
+            ];
+            
+            enhancedSales.push(enhancedSale);
+        }
+        onDisplayDataTable("Sales", headers, enhancedSales);
+    }
+
+    const onDisplayRefillsData = async() => {
+        const headers = ["", "date", "quantity", "expense", "Receipt" ];  
+        const enhancedRefills = [];
+
+        for(let i = 0; i < refills.length; i++) {
+            const refill = refills[i];
+            const expense = await expenseService.getOne(refill.expenseId);
+            
+            const enhancedRefill = [
+                i+1,
+                utils.to_ddMMM(refill.createdAt),
+                refill.quantity,
+                expense.description,
+                expense.photoUrl,
+            ];
+            
+            enhancedRefills.push(enhancedRefill);
+        }
+        onDisplayDataTable("Refills", headers, enhancedRefills);
     }
 
     const fetchItemInfo = async (item) => {
@@ -113,20 +166,40 @@ export default function InventoryScreen({onNavigate, onClose}) {
                                     
                                         <div className="inv-item-body-footer">
                                             <div className="inv-item-body-footer-icon">
+                                                <MinusCircle   
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        onSubtractStock(item);
+                                                    }}
+                                                />
+                                                <p>Subtract</p>
+                                            </div>
+
+                                            <div className="inv-item-body-footer-icon">
                                                 <Receipt   
                                                     onClick={(e) => {
                                                         e.stopPropagation();
-                                                        onDisplayDataTable("Sales", sales);
+                                                        onDisplaySalesData();
                                                     }}
                                                 />
                                                 <p>Sales</p>
+                                            </div>
+
+                                            <div className="inv-item-body-footer-icon">
+                                                <PlusCircle   
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        onAddStock(item);
+                                                    }}
+                                                />
+                                                <p>Add</p>
                                             </div>
                                       
                                             <div className="inv-item-body-footer-icon">
                                                 <ShoppingCart   
                                                     onClick={(e) => {
                                                         e.stopPropagation();
-                                                        onDisplayDataTable("Refills", refills);
+                                                        onDisplayRefillsData();
                                                     }}
                                                 />
                                                 <p>Refills</p>
