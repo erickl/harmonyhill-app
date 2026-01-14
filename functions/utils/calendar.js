8cf7fc5d5c5efe4ec1b2f30f72047c02cca82cbc
Eric Klaesson <ericklaesson@gmail.com>
2026-01-14 15:01:25 -0500

    firestore functions: parse iCal data into the usual checkInAt/checkOutAt fields & add util function to get internal bookings data

--- functions/utils/calendar.js
+++ functions/utils/calendar.js
@@ -1,4 +1,5 @@
 import ICAL from 'ical.js';
+import * as utils from "@harmonyhill/shared/utils.js";
 
 export function parseICal(rawICalData) {
     const jcalData = ICAL.parse(rawICalData);
@@ -8,12 +9,30 @@ export function parseICal(rawICalData) {
 
     const events = vEvents.map(event => {
         const item = new ICAL.Event(event);
+        const checkInAt = utils.toDateTime(item.startDate.toJSDate());
+        const checkOutAt = utils.toDateTime(item.endDate.toJSDate());
         return {
             summary: item.summary,
-            start: item.startDate.toJSDate(),
-            end: item.endDate.toJSDate()
+            checkInAt: checkInAt,
+            checkOutAt: checkOutAt,
         };
     });
 
     return events;
+}
+
+/**
+ * const filtersExample = [
+ *      ["checkOutAt", ">=", today],
+ *      ["house", "==", house],
+ * ];
+ * @param {*} filters 
+ */
+export async function getInternalBookings(filters) {
+    const {makeFirestoreAdapter} = await import("@harmonyhill/shared/firestoreAdapter.js");
+    //const utils = await import("@harmonyhill/shared/utils.js");
+    const {db, Timestamp} = await import("../admin-firebase.js");
+    const adapter = await makeFirestoreAdapter(db, Timestamp);
+    const bookings = await adapter.get("bookings", filters);
+    return bookings;
 }
