b1e6eac4a531c2eb57febcf3de9436c99dbcdf0f
Eline De Buyser <elinedebuyser@Smexmachine-2.local>
2026-06-27 18:22:27 +0200

    delete firebase functions. Moved to new monorepo earlier

--- functions/db-adapter.js
+++
@@ -1,5 +0,0 @@
-import { makeFirestoreAdapter } from "@harmonyhill/shared/firestoreAdapter.js";
-import { db, storage, Timestamp } from "./admin-firebase.js";
-
-// Create the instance once
-export const makeAdapter = async () => await makeFirestoreAdapter(db, storage, Timestamp);
