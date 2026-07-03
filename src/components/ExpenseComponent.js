705d53a0c9cc70af9a648a6e93e4e7a7e9d6bca0
Eline De Buyser <elinedebuyser@Smexmachine-2.local>
2026-07-03 11:30:25 +0200

    extract expense component to a separate component

---
+++ src/components/ExpenseComponent.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import * as utils from "../utils.js";
+import { Pencil, Trash2 } from 'lucide-react';
+import Spinner from './Spinner.js';
+import MetaInfo from './MetaInfo.js';
+import * as bookingService from "../services/bookingService.js";
+import * as activityService from "../services/activityService.js";
+import invoiceLogo from "../assets/invoice-icon.png";
+import { useUserPermissions} from "../context/UserPermissionsContext.js";
+import { useNotification } from "../context/NotificationContext.js";
+import "./ExpenseComponent.css";
+
+export default function ExpenseComponent({expense, handleDelete, onNavigate}) {
+    const [displayedReceipt, setDisplayedReceipt] = useState(null );
+    const [expanded,         setExpanded        ] = useState(false);
+    const [loading,          setLoading         ] = useState(false);
+    const [booking,          setBooking         ] = useState(null);
+    const [activity,         setActivity        ] = useState(null);
+
+    const { permissions} = useUserPermissions();
+    const { onError    } = useNotification();
+
+    const fetchBookingInfo = async (expense) => {
+        if(!utils.isEmpty(expense.bookingId)) {
+            const booking_ = await bookingService.getOne(expense.bookingId);
+            setBooking(booking_)
+
+            if(expense.activityId) {
+                const activity_ = await activityService.getOne(expense.bookingId, expense.activityId);
+                setActivity(activity_)  
+            }
+        }
+        
+    };
+
+    const handleSetExpanded = async(expense) => {
+        setLoading(prev => !prev);     
+        await fetchBookingInfo(expense);
+        setLoading(prev => !prev);
+        setExpanded(prev => !prev);
+    }
+
+    return (
+        <div className="expense-box" onClick={()=> handleSetExpanded(expense)}>
+            <div className="expense-header">
+                <div className="expense-header-left">
+                    <div className="expense-title">
+                        {`${expense.index}. ${utils.capitalizeWords(expense.description)}`}
+                    </div>
+                </div>
+                <div className="expense-header-right">
+                    <div>
+                        {utils.formatDisplayPrice(expense.amount, true)}
+                    </div>
+                    <div className="expand-icon">
+                        {expanded ? '▼' : '▶'}
+                    </div>
+                </div>
+            </div>  
+            
+            <div>
+                {utils.to_ddMMYY(expense.purchasedAt, "/")}
+            </div>
+
+            { expanded && loading ? (
+                <Spinner />
+            ) : expanded && !loading ? (
+                <div className="expense-body">
+                    <div>
+                        Payment Method: {utils.capitalizeWords(expense.paymentMethod)}
+                    </div>
+                    <div>
+                        Category: {utils.capitalizeWords(expense.category)}
+                    </div>
+                    {booking && (<div>
+                        Booking: {booking.name}
+                    </div>)}
+                    {activity && (<div>
+                        Activity: {activity.name}
+                    </div>)}
+                    {expense.comments && (<div>
+                        Comments: {expense.comments}
+                    </div>)}
+                    <div>
+                        Purchased By: {utils.capitalizeWords(expense.purchasedBy)}
+                    </div>
+                    <div className="expense-body-footer">
+                        <div className="expense-body-footer-icon">
+                            <Pencil   
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    onNavigate("edit-expense", {expenseToEdit:expense})
+                                }}
+                            />
+                            <p>Edit</p>
+                        </div>
+                        {permissions.isManagerOrAdmin && (
+                            <div className="expense-body-footer-icon">
+                                <Trash2  
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        handleDelete(expense);
+                                    }}
+                                />
+                                <p>Delete</p>
+                            </div>
+                        )}
+                        <div className="expense-body-footer-icon">
+                            <img 
+                                className="expense-thumbnail" 
+                                //src={expense.thumbNailUrl} 
+                                src={invoiceLogo} 
+                                alt={`preview-${expense.id}`} 
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setDisplayedReceipt(expense);
+                                }}
+                            />
+                            <p>Receipt</p>
+                        </div>
+                    </div>
+                    <MetaInfo document={expense}/>
+                </div>
+            ) : (<></>)}
+
+            {displayedReceipt && (
+                <img 
+                    className="expense-photo" 
+                    src={displayedReceipt.photoUrl} 
+                    alt={`expense-${displayedReceipt.id}`} 
+                    onClick={() => setDisplayedReceipt(null)}
+                />
+            )}
+        </div>
+    );
+}
--- src/components/ExpensesScreen.js
+++ src/components/ExpensesScreen.js
@@ -2,19 +2,16 @@ import React, { useState, useEffect } from 'react';
 import * as expenseService from "../services/expenseService.js";
 import { useNotification } from "../context/NotificationContext.js";
 import * as utils from "../utils.js";
-import * as bookingService from "../services/bookingService.js";
-import * as activityService from "../services/activityService.js";
 import { useUserPermissions} from "../context/UserPermissionsContext.js";
 import * as ledgerService from "../services/ledgerService.js";
+import * as issueService from "../services/issueService.js";
 import "./ExpensesScreen.css";
 import VeganHamburgerButton from './VeganHamburgerButton.js';
-import Spinner from "./Spinner.js";
-import invoiceLogo from "../assets/invoice-icon.png";
 import { useConfirmationModal } from "../context/ConfirmationContext.js";
 import { useSuccessNotification } from "../context/SuccessContext.js";
-import { Pencil, Trash2, ImageDown } from 'lucide-react';
+import { ImageDown } from 'lucide-react';
 import SheetUploader from "./SheetUploader.js";
-import MetaInfo from './MetaInfo.js';
+import ExpenseComponent from "./ExpenseComponent.js";
 import { useFilters } from "../context/FilterContext.js";
 import { useProgressCounter } from "../context/ProgressContext.js";
 
@@ -23,7 +20,7 @@ export default function ExpensesScreen({ onNavigate, onClose }) {
     const [expandedExpenses, setExpandedExpenses] = useState({}   );
     const [loadingExpanded,  setLoadingExpanded ] = useState({}   );
     const [expenses,         setExpenses        ] = useState([]   );
-    const [displayedReceipt, setDisplayedReceipt] = useState(null );
+    const [issues,           setIssues          ] = useState([]   );
     const [loading,          setLoading         ] = useState(true );
     const [pettyCash,        setPettyCash       ] = useState(null );
     const [expenseSum,       setExpenseSum      ] = useState(null );
@@ -40,12 +37,6 @@ export default function ExpensesScreen({ onNavigate, onClose }) {
         "before" : "date",
         "paymentMethod" : "string",
     };
-
-    const handleSetExpanded = async(expense) => {
-        setLoadingExpanded((prev) => ({...prev, [expense.id]: true}));
-        await fetchBookingInfo(expense);
-        setLoadingExpanded((prev) => ({...prev, [expense.id]: false}));
-    }
 
     const getDataForExport = async(filterValues, onProgress) => {
         const rows = await expenseService.toArrays(filterValues, onProgress, onError);
@@ -64,28 +55,6 @@ export default function ExpensesScreen({ onNavigate, onClose }) {
         const success = await expenseService.downloadExpenseReceipts(filename, filters, onProgress, onError);
         const x = 1; // todo, if success = true, display onSuccess?
     }
-
-    const fetchBookingInfo = async (expense) => {
-        let updatedExpandedList = { ...(expandedExpenses || {}) };
-        
-        const expand = utils.isEmpty(updatedExpandedList[expense.id]);
-        if(expand) {
-            if(!utils.isEmpty(expense.bookingId)) {
-                const booking = await bookingService.getOne(expense.bookingId);
-                expense.bookingName = booking ? booking.name: "missing booking";
-
-                if(expense.activityId) {
-                    const activity = await activityService.getOne(expense.bookingId, expense.activityId);
-                    expense.activityName = activity ? activity.displayName : "missing activity";
-                }
-            }
-            updatedExpandedList[expense.id] = expense;
-        } else {
-            updatedExpandedList[expense.id] = null;
-        }
-
-        setExpandedExpenses(updatedExpandedList);
-    };
 
     const handleDeleteExpense = async(expenseToDelete) => {
         if(!expenseToDelete || utils.isEmpty(expenseToDelete.id)) return;
@@ -107,7 +76,7 @@ export default function ExpensesScreen({ onNavigate, onClose }) {
 
             const filter = lastClosedPettyCashRecord ? { "after" : lastClosedPettyCashRecord.closedAt} : {};
             
-            if(!permissions.sAdmin) {
+            if(!permissions.isAdmin) {
                 // While the manager just is concerned with petty cash, he has no reason to see all bank transfers
                 filter["paymentMethod"] = "cash";
             } 
@@ -121,6 +90,9 @@ export default function ExpensesScreen({ onNavigate, onClose }) {
             setExpenseSum(expenseSum);
 
             setLoading(false);
+
+            const issues = await issueService.get("expenses", {}, onError);
+            setIssues(issues);
         }
 
         fetchExpenses();
@@ -160,101 +132,15 @@ export default function ExpensesScreen({ onNavigate, onClose }) {
                 {expenses.map((expense) => {
                     return (
                         <React.Fragment key={expense.id}>
-                            <div className="expense-box" onClick={()=> handleSetExpanded(expense)}>
-                                <div className="expense-header">
-                                    <div className="expense-header-left">
-                                        <div className="expense-title">
-                                            {`${expense.index}. ${utils.capitalizeWords(expense.description)}`}
-                                        </div>
-                                    </div>
-                                    <div className="expense-header-right">
-                                        <div>
-                                            {utils.formatDisplayPrice(expense.amount, true)}
-                                        </div>
-                                        <div className="expand-icon">
-                                            {expandedExpenses[expense.id] ? '▼' : '▶'}
-                                        </div>
-                                    </div>
-                                </div>  
-                                
-                                <div>
-                                    {utils.to_ddMMYY(expense.purchasedAt, "/")}
-                                </div>
-
-                                {loadingExpanded?.[expense.id] === true ? (
-                                    <Spinner />
-                                ) : expandedExpenses?.[expense.id] ? (
-                                    <div className="expense-body">
-                                        <div>
-                                            Payment Method: {utils.capitalizeWords(expense.paymentMethod)}
-                                        </div>
-                                        <div>
-                                            Category: {utils.capitalizeWords(expense.category)}
-                                        </div>
-                                        {expense.bookingName && (<div>
-                                            Booking: {expense.bookingName}
-                                        </div>)}
-                                        {expense.activityName && (<div>
-                                            Activity: {expense.activityName}
-                                        </div>)}
-                                        {expense.comments && (<div>
-                                            Comments: {expense.comments}
-                                        </div>)}
-                                        <div>
-                                            Purchased By: {utils.capitalizeWords(expense.purchasedBy)}
-                                        </div>
-                                        <div className="expense-body-footer">
-                                            <div className="expense-body-footer-icon">
-                                                <Pencil   
-                                                    onClick={(e) => {
-                                                        e.stopPropagation();
-                                                        onNavigate("edit-expense", {expenseToEdit:expense})
-                                                    }}
-                                                />
-                                                <p>Edit</p>
-                                            </div>
-                                            {permissions.isManagerOrAdmin && (
-                                                <div className="expense-body-footer-icon">
-                                                    <Trash2  
-                                                        onClick={(e) => {
-                                                            e.stopPropagation();
-                                                            handleDeleteExpense(expense);
-                                                        }}
-                                                    />
-                                                    <p>Delete</p>
-                                                </div>
-                                            )}
-                                            <div className="expense-body-footer-icon">
-                                                <img 
-                                                    className="expense-thumbnail" 
-                                                    //src={expense.thumbNailUrl} 
-                                                    src={invoiceLogo} 
-                                                    alt={`preview-${expense.id}`} 
-                                                    onClick={(e) => {
-                                                        e.stopPropagation();
-                                                        setDisplayedReceipt(expense);
-                                                    }}
-                                                />
-                                                <p>Receipt</p>
-                                            </div>
-                                        </div>
-                                        <MetaInfo document={expense}/>
-                                    </div>
-                                ) : (<></>)}
-                            </div>
+                            <ExpenseComponent 
+                                expense={expense} 
+                                handleDelete={handleDeleteExpense}
+                                onNavigate={onNavigate}
+                            />
                         </React.Fragment>
                     )
                 })}
             </div>
-
-            {displayedReceipt && (
-                <img 
-                    className="expense-photo" 
-                    src={displayedReceipt.photoUrl} 
-                    alt={`expense-${displayedReceipt.id}`} 
-                    onClick={() => setDisplayedReceipt(null)}
-                />
-            )}
         </div>
     )
 }
